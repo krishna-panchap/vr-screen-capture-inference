89139bdc6d2ded3cdaed32481c637ce76f996765
Zack <zack@ukaton.com>
2023-08-12 13:12:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 import websockets
 import threading
 import json
+from time import sleep
 
 from ultralytics import YOLO
 from mss import mss
@@ -36,18 +37,64 @@ ssl_keyfile = get_path("sec/private_key.key")
 
 websocket_clients = set()
 
-camera_position = [0, 0, 0]
-camera_rotation = [0, 0, 0]
+camera_position = None
+camera_rotation = None
 
 screenshots = []
 screenshot_positions = []
 screenshot_rotations = []
 
 
-# used to determine if a new screenshot should be saved, and retrieve screenshots for "zooming in"
+# ChatGPT
+def difference_lists(list1, list2, abs_difference=False):
+    diffs = [abs(a - b) if abs_difference else a -
+             b for a, b in zip(list1, list2)]
+    return diffs
+
+
+rotation_thresholds = [0.1, 0.1]
+
+
+def is_rotation_difference_within_thresholds(rotation):
+    return rotation[0] < rotation_thresholds[0] and rotation[1] < rotation_thresholds[1]
+
+
+def is_rotation_valid(rotation1, rotation2):
+    rotation_difference = difference_lists(rotation1, rotation2, True)
+    return is_rotation_difference_within_thresholds(rotation_difference)
+
+
+distance_threshold = 0.1
+distance_threshold_squared = distance_threshold**2
+
+
+def get_position_difference(position, origin):
+    return difference_lists(position, origin)
+
+
+def get_position_squared_length(position, origin):
+    diff = get_position_difference(position, origin)
+    squared_length = 0
+    for n in diff:
+        squared_length += n**2
+    return squared_length
+
+
 def find_closest_screenshot_index(position: [float], rotation: [float]) -> int:
     closest_screenshot_index = -1
-    # FILL
+    number_of_screenshots = len(screenshots)
+    lowest_length = distance_threshold
+    for index in range(number_of_screenshots):
+        screenshot_position = screenshot_positions[index]
+        squared_distance = get_position_squared_length(
+            screenshot_position, position)
+        if squared_distance < lowest_length:
+            screenshot_rotation = screenshot_rotations[index]
+            rotation_is_valid = is_rotation_valid(
+                rotation, screenshot_rotation)
+            if rotation_is_valid:
+                lowest_length = squared_distance
+                closest_screenshot_index = index
     return closest_screenshot_index
 
 
@@ -138,7 +185,6 @@ async def setup_yolo():
                 screenshot = sct.grab(monitor)
                 img = Image.frombytes(
                     'RGB', (monitor["width"]*pixel_scalar, monitor["height"]*pixel_scalar), screenshot.rgb)
-                # FILL - save screenshot if a unique position/orientation
                 screenshot_array = np.array(img)
                 screen = cv2.cvtColor(screenshot_array, cv2.COLOR_RGB2BGR)
                 results = model.track(
@@ -178,6 +224,34 @@ async def setup_yolo():
             pass
 
 
+screenshot_loop_interval = 1.0
+
+
+def screenshot_loop():
+    with mss() as sct:
+        try:
+            while True:
+                if camera_position is not None:
+                    closest_screenshot_index = find_closest_screenshot_index(
+                        camera_position, camera_rotation)
+                    should_add_screenshot = False
+                    if closest_screenshot_index == -1:
+                        should_add_screenshot = True
+                        pass
+                    else:
+                        # if there's an existing "close" screenshot - should it be replaced?
+                        pass
+
+                    if should_add_screenshot:
+                        screenshot = sct.grab(monitor)
+                        screenshot_positions.append(camera_position.copy())
+                        screenshot_rotations.append(camera_rotation.copy())
+                        screenshots.append(screenshot)
+                sleep(screenshot_loop_interval)
+        except KeyboardInterrupt:
+            pass
+
+
 def main():
 
     # Start HTTPS server in a new thread
@@ -188,11 +262,15 @@ def main():
     websocket_thread = threading.Thread(target=setup_websocket_server)
     websocket_thread.start()
 
+    screenshot_loop_thread = threading.Thread(target=screenshot_loop)
+    screenshot_loop_thread.start()
+
     # asyncio.run(setup_yolo())
 
     # Wait for the WebSocket server thread to finish (which will be never unless stopped manually)
     websocket_thread.join()
     https_server_thread.join()
+    screenshot_loop_thread.join()
 
 
 if __name__ == "__main__":
